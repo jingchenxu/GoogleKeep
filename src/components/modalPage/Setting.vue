457345fb6798116a8b00994b0b0aef14a6f6579b
jcxu <jcxu@jinhetech.com>
2018-10-03 23:48:23 +0800

    (feature)完成默认浏览器打开跳转链接

--- src/components/modalPage/Setting.vue
+++ src/components/modalPage/Setting.vue
@@ -4,18 +4,62 @@
       <v-card-title>
         <span class="headline">设置</span>
       </v-card-title>
-      <v-card-text>
+      <v-card-text style="padding-top: 0;">
         <div class="item-container">
           <v-divider></v-divider>
           <span class="grey--text">清单运作方式</span><br>
-          <span>Whitehaven Beach<v-checkbox v-model="checkbox2"></v-checkbox></span><br>
-          <span>Whitsunday Island, Whitsunday Islands</span>
+          <v-layout align-center>
+            <span>将清单项添加至底部</span>
+            <v-spacer></v-spacer>
+            <v-checkbox style="margin-top: 0; padding-top: 0;" v-model="isShare" hide-details class="shrink"></v-checkbox>
+          </v-layout>
+          <v-layout align-center>
+            <span>将已勾选的清单项移到底部</span>
+            <v-spacer></v-spacer>
+            <v-checkbox style="margin-top: 0; padding-top: 0;" v-model="isShare" hide-details class="shrink"></v-checkbox>
+          </v-layout>
+        </div>
+        <div class="item-container">
+          <v-divider></v-divider>
+          <span class="grey--text">自定义默认提醒时间</span><br>
+          <v-layout align-center>
+            <span>上午</span>
+            <v-spacer></v-spacer>
+            <v-text-field label="Regular"></v-text-field>
+          </v-layout>
+          <v-layout align-center>
+            <span>下午</span>
+            <v-spacer></v-spacer>
+            <v-text-field label="Regular"></v-text-field>
+          </v-layout>
+          <v-layout align-center>
+            <span>傍晚</span>
+            <v-spacer></v-spacer>
+            <v-text-field label="Regular"></v-text-field>
+          </v-layout>
+        </div>
+        <div class="item-container">
+          <v-divider></v-divider>
+          <v-layout align-center>
+            <span>启用共享功能</span>
+            <v-spacer></v-spacer>
+            <v-checkbox style="margin-top: 0; padding-top: 0;" v-model="isShare" hide-details class="shrink"></v-checkbox>
+          </v-layout>
+          <!-- <span>启用共享功能<v-checkbox v-model="isShare" hide-details class="shrink mr-2"></v-checkbox></span> -->
+        </div>
+        <div class="item-container">
+          <v-divider></v-divider>
+          <v-layout align-center>
+            <span>显示富媒体链接预览</span>
+            <v-spacer></v-spacer>
+            <v-checkbox style="margin-top: 0; padding-top: 0;" v-model="isRichText" hide-details class="shrink"></v-checkbox>
+          </v-layout>
         </div>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="grey" flat @click.native="dialog = false">取消</v-btn>
-        <v-btn color="grey" flat @click.native="dialog = false">保存</v-btn>
+        <v-btn flat @click.native="dialog = false">取消</v-btn>
+        <v-btn color="blue" flat @click.native="dialog = false">保存</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -27,7 +71,9 @@ export default {
   data () {
     return {
       dialog: false,
-      checkbox2: false
+      checkbox2: false,
+      isShare: false,
+      isRichText: false
     }
   },
   mounted () {
@@ -44,6 +90,6 @@ export default {
 
 }
 .item-container {
-  line-height: 36px;
+  line-height: 48px;
 }
 </style>
